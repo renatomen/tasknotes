d37b280f94002d81dc1d8ad964e7f691d510fc70
tvoklov <tvoklov@gmail.com>
2025-11-21 20:04:14 +0400

    refactor

--- src/bases/KanbanView.ts
+++ src/bases/KanbanView.ts
@@ -874,14 +874,14 @@ export class KanbanView extends BasesViewBase {
 		const task = await this.plugin.cacheManager.getTaskInfo(taskPath);
 		const taskProperty = this.plugin.fieldMapper.lookupMappingKey(frontmatterKey);
 
-		if (!task || !taskProperty || !(taskProperty in task)) {
+		if (task && taskProperty && taskProperty in task) {
+			// Update the task property
+			await this.plugin.taskService.updateProperty(task, taskProperty as keyof TaskInfo, value);
+		} else {
 			// Update the frontmatter directly
 			await this.plugin.app.fileManager.processFrontMatter(file, (frontmatter) => {
 				frontmatter[frontmatterKey] = value;
 			});
-		} else {
-			// Update the task property
-			await this.plugin.taskService.updateProperty(task, taskProperty as keyof TaskInfo, value);
 		}
 	}
 
